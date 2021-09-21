bac04331307f2d307fef8bf078c72abcb3647a12
NeelPatelcoder <neelp.mobio@gmail.com>
2021-09-21 14:41:57 +0530

    done

---
+++ helpers/baseUrl.js
@@ -0,0 +1,7 @@
+// const baseUrl = process.env.NODE_ENV === 'production' ? "" :"http://localhost:3000"
+
+// export default baseUrl
+
+   
+const baseUrl = process.env.NODE_ENV === 'production' ? "https://mystore202144.vercel.app": "http://localhost:3000"
+export default baseUrl
